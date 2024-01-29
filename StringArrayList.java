28a2a4c248a42fe3d733af9818f6133aca523c77
jackaakash4 <jackaakash4@gmail.com>
2024-01-29 12:29:43 +0545

    ArrayList: add, set, remove

--- StringArrayList.java
+++ StringArrayList.java
@@ -9,6 +9,18 @@ public class StringArrayList{
 		al.add(1, "Aakash");
 
 		System.out.println(al);
+
+		//setting element in 1st index
+		al.set(1, "Shrestha");
+		System.out.println("After setting i.e. set() : " + al);
+
+		//removing element 
+		//removing from index
+		al.remove(1);
+
+		System.out.println("After removing 1st index element "+ al);
+	
 	}
+
 }
 
