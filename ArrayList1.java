6443715b0c5f3519503ebf8f80ca7f9fd6f9f007
jackaakash4 <jackaakash4@gmail.com>
2024-07-29 10:11:06 +0545

    Learned: ArrayList

--- ArrayList1.java
+++ ArrayList1.java
@@ -1,10 +1,17 @@
-//multi-dimentional arrayList
-
-
+import java.util.*;
 import java.util.ArrayList;
 
-public class MultiDArrayList{
-	public static void main(String[] args){
-		//multi-dimentional ArrayList
-		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-		
+class ArrayList1{
+	public static void main(String args[]){
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Jack");
+		al.add("Aakash");
+
+		System.out.println(al);
+
+		al.add(1, "Mr.");
+
+		System.out.println(al);
+	}
+}
+
